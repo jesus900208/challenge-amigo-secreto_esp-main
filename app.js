86f3bfd75aedbabe4af98f9d5d6fa3f2104d90fe
Jesús <jesus_js90@outlook.com>
2025-01-30 04:58:38 -0600

    Actualiza el app

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista_amigos = [];
+
+function agregarAmigo() {
+    let amigoEntrada = document.getElementById('amigo').value;
+    
+}
